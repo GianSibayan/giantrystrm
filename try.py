bf5eaa71d49788e341c56b4f09273b1d538603b5
GianSibayan <81125753+GianSibayan@users.noreply.github.com>
2024-11-14 21:25:50 +0800

    Update try.py

--- try.py
+++ try.py
@@ -39,7 +39,7 @@ from sklearn.metrics import silhouette_score
 import matplotlib.pyplot as plt
 
 # Download NLTK resources
-nltk.download('vade_lexicon')
+nltk.download('vader_lexicon')
 nltk.download('punkt')
 nltk.download('punkt_tab')
 nltk.download('stopwords')
